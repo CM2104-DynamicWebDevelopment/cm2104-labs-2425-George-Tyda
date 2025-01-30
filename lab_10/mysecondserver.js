b77551782f1bfc6e867608397a58a9e5f800f989
George Tidakabi <georgetyda@gmail.com>
2025-01-30 11:33:37 +0000

    new

--- lab_10/mysecondserver.js
+++ lab_10/mysecondserver.js
@@ -1,7 +1,9 @@
 
 var http = require('http');
+var currentdate = require('./mymodule');
 
 http.createServer(function (req, res) {
+    res.whiteHead(200, {'Content-Type': 'text/html'});
 
  res.writeHead(200, {'Content-Type': 'text/html'});
  res.end('Hello World!');
