59e897b0711ab3e1aa5a7d0202964a241721acc4
George Tidakabi <georgetyda@gmail.com>
2025-01-30 11:51:19 +0000

    new

--- lab_10/mythirdserver.js
+++ lab_10/mythirdserver.js
@@ -5,7 +5,7 @@ var knockknock = require('knock-knock-jokes');
 http.createServer(function (req, res) {
     res.whiteHead(200, {'Content-Type': 'text/html'});
     var randomJoke = knocknock()
-    res.end('randomJoke');
+    res.end(randomJoke);
 
  
 }).listen(8080);
